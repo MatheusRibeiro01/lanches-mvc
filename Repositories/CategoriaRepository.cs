6c33678137a3c7d59f32aa6e23fcfdcb01959b50
Matheus Ribeiro <matheusgomes612@gmail.com>
2021-07-17 16:59:08 -0300

    Implementando padrão repository

---
+++ Repositories/CategoriaRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Lanches.Context;
+using Lanches.Models;
+
+namespace Lanches.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+        public CategoriaRepository(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        public IEnumerable<Categoria> Categorias => _context.Categorias;
+    }
+}
---
+++ Repositories/LancheRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Lanches.Context;
+using Lanches.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lanches.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+        public LancheRepository(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(c => c.categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches.Where(p =>
+            p.IsLanchePreferido).Include(c => c.categoria);
+        public Lanche GetLancheById(int lancheId)
+        {
+            return _context.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+        }
+    }
+}
